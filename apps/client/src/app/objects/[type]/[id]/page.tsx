349dd53208ffec455e1c5f14e1f23e6688d9bb70
Murad Akhmedkhanov <muradahmedhanov8779@gmail.com>
2026-03-18 14:27:49 +0300

    feat(client): add logging of object keys for debugging2

--- apps/client/src/app/objects/[type]/[id]/page.tsx
+++ apps/client/src/app/objects/[type]/[id]/page.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 import { MapPin } from 'lucide-react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import SellRentForm from '@/components/forms/SellRentForm';
 import { ExtendedDataDisplay } from '@/components/forms/ExtendedDataDisplay'; // путь к вашему скопированному файлу
